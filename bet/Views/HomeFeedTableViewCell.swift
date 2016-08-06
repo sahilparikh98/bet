3da4f806ffd9cab1e22c52b6beb0b48ceedf5ba0
sahilparikh98 <sparikh98@gmail.com>
2016-08-06 00:59:22 -0700

    work on card UI

---
+++ bet/Views/HomeFeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeFeedTableViewCell.swift
+//  bet
+//
+//  Created by Apple on 8/6/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+class HomeFeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
